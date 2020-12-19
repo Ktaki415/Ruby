1231428fa76cf79bc09c074f6217c571a7922273
瀧村郁 <k.taki415@gmail.com>
2020-12-19 21:20:10 +0900

    [Add] roop1

---
+++ ruby_practice/roop.rb
@@ -0,0 +1,39 @@
+# for inによるループ処理
+
+for i in 5..10 # 0, 1, 2, ...4
+    puts "hello world #{i}" # 繰り返し処理
+end
+
+puts "last #{i}"
+
+# for in によるループ処理
+for i in 0..4
+    puts "ハロー、paizaラーニング"
+end
+
+# for in によるループ処理
+# 数値を0から15まで表示する
+for i in 0..15
+    puts i
+end
+
+# 1月から12月まで表示する
+for i in 1..12
+    puts "#{i}月"
+end
+
+
+# whileで文章を5回繰り返す
+i = 0
+while i <= 4
+    puts "ハロー、paizaラーニング"
+    i = i + 1
+end
+
+# 数値を0から15まで表示する
+# whileによるループ処理
+i = 0
+while i <= 15
+    puts i
+    i = i + 1
+end
